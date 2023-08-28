56b7d0100db1a82998beef73b92eec2fe1d929f9
Muhammad Zulkifal <100269142+mzulkifalzk25@users.noreply.github.com>
2023-08-28 20:40:59 +0500

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -67,18 +67,17 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "blog.wsgi.application"
+WSGI_APPLICATION = "blog.wsgi.app"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {}
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 
 
 # Password validation
